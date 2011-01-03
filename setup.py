590fd4cafaa1eaca3d082480326b9c844357c8e0
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2011-01-03 14:03:39 +0000

    The next release will be 0.9

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (
 
 setup(
     name='zope.fanstatic',
-    version='0.8dev',
+    version='0.9dev',
     description="Fanstatic integration for Zope.",
     long_description=long_description,
     classifiers=['Framework :: Zope3'],
