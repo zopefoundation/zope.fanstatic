b31bb9adeeca6ec955497195153e80afcb52de67
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2011-01-19 14:55:09 +0000

    Conform to repository policy.

--- src/zope/fanstatic/tests/test_computeurl.py
+++ src/zope/fanstatic/tests/test_computeurl.py
@@ -1,5 +1,17 @@
+##############################################################################
+#
+# Copyright (c) 2011 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import unittest
-from zope.interface import Interface
 from zope.component import getMultiAdapter
 from zope.publisher.browser import TestRequest
 from zope.traversing.interfaces import ITraversable
