aaa15b0c987d5a778e43b98853e4f7e0e43fa920
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-08-17 10:11:32 +0000

    Preparing release 0.11

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (
 
 setup(
     name='zope.fanstatic',
-    version='0.11dev',
+    version='0.11',
     description="Fanstatic integration for Zope.",
     long_description=long_description,
     classifiers=['Framework :: Zope3'],
